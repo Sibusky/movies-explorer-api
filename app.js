49aeafe7ce30848d6b9db0ac517bd6e0d8cc6cf7
Alexey Smirnov <Sibusky@yandex.ru>
2022-11-09 13:08:44 +0400

    feat: add userschema

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const { PORT = 3000 } = process.env;
+
+// Подключаю БД
+
+// Валидирую данные данные
+
+// Превращаю тело запроса в удобный формат JSON
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Разрешаю CORS
+
+// Подключаю логгер запросов
+
+// Краш-тест. После код-ревью удалить.
+
+// Роуты не требующие авторизации: логин и регистрация
+
+// Роут авторизации
+
+// Роуты, требующие авторизации
+
+// Роут на ненайденную страницу
+
+// Подключаю логгер ошибок
+
+// Мидлвара celebrate для отправки ошибки пользователю
+
+// Обработчик ошибок
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.json(req.headers);
+})
+
+app.listen(PORT, () => {
+  console.log('App started and listen port', PORT);
+});
